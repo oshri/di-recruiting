236362f3f8b99a05cd23a05c0f8fbcb46e1ac883
Oshri Kdoshim <oshri.kdoshim@citi.com>
2018-05-02 04:00:05 +0300

    add quoteSection

---
+++ src/components/QuoteSection/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import LazyLoad from 'react-lazy-load';
+import PropTypes from 'prop-types';
+import './QuoteSection.css';
+
+function QuoteSection(props) {
+	const { quote, imageUrl } = props;
+
+	return (
+		<div className="Quote">
+            {/* <div className="border-decorator">
+                <span className="bd lt"></span>
+                <span className="bd rt"></span>
+                <span className="bd lb"></span>
+                <span className="bd rb"></span>
+            </div> */}
+            <div className="BoxWrap">
+                <h4>    
+                    <span>{quote.p1}</span>
+                    <strong>{quote.p2}</strong>
+                    <span>{quote.p3}</span>
+                </h4>
+            </div>
+			<LazyLoad className="quoteImage" offsetVertical={300}>
+				<img src={imageUrl} />
+			</LazyLoad>
+		</div>
+	);
+}
+
+export default QuoteSection;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,16 +3,23 @@ import LazyLoad from 'react-lazy-load';
 import { Parallax, Background } from 'react-parallax';
 import './Home.css';
 import Signature from '../../components/Signature';
+import QuoteSection from '../../components/QuoteSection';
 
 // cvUIv9j5wDg  for contact
-
+// SYTO3xs06fU
 class Home extends Component {
     splash = "https://source.unsplash.com/xah3Ny20Vbw/400";
+    quoteImageUrl = "https://source.unsplash.com/SYTO3xs06fU/400";
+    quote = {
+        p1: 'Sourcing is the ',
+        p2: 'key to your',
+        p3: 'recruitmentnstrategy'
+    };
 
 	render() {
 		return (
             <div style={this.styles}>
-                <Parallax bgImage={this.splash} strength={500}>
+                <Parallax bgImage={this.splash}>
                     <div style={{height: 700}}>
                         <div className="text">
                             "Sourcing should be viewed as a business partner, someone who is critical to the success of the business."
@@ -27,9 +34,7 @@ class Home extends Component {
                     <div className="text">Dynamic Blur</div>
                 </div>
                 </Parallax>
-                <div style={{height: 500}}>
-
-                </div>
+                <QuoteSection quote={this.quote} imageUrl={this.quoteImageUrl} ></QuoteSection>
                 <Parallax strength={-100}>
                 <div style={{height: 500}}>
                     <div className="text">Reverse direction</div>
